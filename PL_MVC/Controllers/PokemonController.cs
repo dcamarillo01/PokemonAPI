ff4a54a08bcaaa46c59b8bcd0590b14a2e50303a
dcamarillo01 <dcamarillo01@hotmail.com>
2025-08-22 18:24:37 -0600

    Add project files.

---
+++ PL_MVC/Controllers/PokemonController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL_MVC.Controllers
+{
+    public class PokemonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Pokemones() { 
+        
+            return View();
+        }
+    }
+}
